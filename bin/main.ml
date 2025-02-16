c550e7402f475e19218cf56634a2800c3778593c
Tommy Kolb <tommyjkolb@gmail.com>
2025-02-16 11:51:42 -0500

    test cases for checking duplicate class definitions; structure of how to create class map

--- bin/main.ml
+++ bin/main.ml
@@ -17,6 +17,9 @@ let get_file_contents file_dir : string list =
     close_in handle;
     contents
 
+let construct_class_map ast : Ast.ast_class list = 
+    []
+
 let () = 
     let rec print_contents arr = 
         match arr with
@@ -28,4 +31,5 @@ let () =
     
     let file_contents = get_file_contents Sys.argv.(1) in
     let ast = Ast.parse_ast file_contents in
+    let class_map = construct_class_map ast in
     ()
