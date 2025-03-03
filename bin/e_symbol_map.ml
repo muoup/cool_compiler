bb784734f1a886dbcac9e5b0f09300a68fe8cb7b
Tommy Kolb <tommyjkolb@gmail.com>
2025-03-03 15:03:37 -0500

    We were getting 0 because apparently Hashtbl.Make wasn't allowed

--- bin/e_symbol_map.ml
+++ bin/e_symbol_map.ml
@@ -1,16 +1,11 @@
-module Symbol_data = Hashtbl.Make(struct
-    type t = string
-    let equal = String.equal
-    let hash = String.hash
-end)
-
+(* Define a standard hashtable for symbols *)
 type symbol_map = {
-    data : string Symbol_data.t;
+    data : (string, string) Hashtbl.t;
     scoped_data : string list list
 }
 
 let new_symbol_map () : symbol_map =
-    { data = Symbol_data.create 100; scoped_data = [[]] }
+    { data = Hashtbl.create 100; scoped_data = [[]] }
 
 let push_scope (map : symbol_map) : symbol_map =
     { scoped_data = [] :: map.scoped_data; data = map.data }
@@ -19,7 +14,7 @@ let pop_scope (map : symbol_map) : symbol_map =
     let hd = List.hd map.scoped_data
     and tl = List.tl map.scoped_data in
 
-    List.iter (fun (var_name : string) -> Symbol_data.remove map.data var_name) hd;
+    List.iter (fun (var_name : string) -> Hashtbl.remove map.data var_name) hd;
 
     { scoped_data = tl; data = map.data }
 
@@ -30,36 +25,36 @@ let add_symbol (var_name : string) (var_type : string) (map : symbol_map) : symb
     let new_scope = (var_name :: hd) :: tl in
     let new_map = { scoped_data = new_scope; data = map.data } in
 
-    Symbol_data.add map.data var_name var_type;
+    Hashtbl.add map.data var_name var_type;
 
     new_map
 
 let get_symbol (var_name : string) (map : symbol_map) : string =
-    Symbol_data.find map.data var_name
+    Hashtbl.find map.data var_name
 
 let _basic_sym_map_test () =
-    let map = new_symbol_map () in
+  let map = new_symbol_map () in
 
-    let map = push_scope map in
-    let map = add_symbol "a" "Int" map in
-    let map = add_symbol "b" "String" map in
+  let map = push_scope map in
+  let map = add_symbol "a" "Int" map in
+  let map = add_symbol "b" "String" map in
 
-    let a = get_symbol "a" map in
-    let b = get_symbol "b" map in
+  let a = get_symbol "a" map in
+  let b = get_symbol "b" map in
 
-    assert (a = "Int");
+  assert (a = "Int");
 
-    assert (b = "String");
+  assert (b = "String");
 
-    let map = push_scope map in
-    let map = add_symbol "a" "String" map in
+  let map = push_scope map in
+  let map = add_symbol "a" "String" map in
 
-    let a = get_symbol "a" map in
-    assert (a = "String");
+  let a = get_symbol "a" map in
+  assert (a = "String");
 
-    let map = pop_scope map in
+  let map = pop_scope map in
 
-    let a = get_symbol "a" map in
-    assert (a = "Int");
+  let a = get_symbol "a" map in
+  assert (a = "Int");
 
-    Printf.printf "Symbol Map Test Passed\n"
+  Printf.printf "Symbol Map Test Passed\n"
