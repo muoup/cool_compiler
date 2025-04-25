1648f2fa8cb4183c929476c76e291adb9113650f
Tommy Kolb <tommyjkolb@gmail.com>
2025-04-25 12:04:44 -0400

    Some dead code is finally being eliminated (not all obviously)

--- compiler/bin/h_dead_code_elim.ml
+++ compiler/bin/h_dead_code_elim.ml
@@ -1,90 +1,88 @@
 open D_tac_data
 open G_tac_to_cfg
 
-let rec overwrites_before_usage (id : tac_id) (lst : tac_cmd list) : bool = 
-  match lst with 
-  | [] -> false
-  | hd :: tl -> (
-    Printf.printf "Checking if id is overwritten %s \n" (p_id id);
-      Printf.printf "by instruction %s \n" (string_of_tac_cmd hd);
-      match hd with 
-      | TAC_add     (left, a, b)
-      | TAC_sub     (left, a, b)
-      | TAC_mul     (left, a, b)
-      | TAC_div     (_, left, a, b)
-      | TAC_lt      (left, a, b)
-      | TAC_lte     (left, a, b)
-      | TAC_eq      (left, a, b) -> (
-        if (id = left && id != a && id != b) then true 
-        else if (id = a || id = b) then false
-        else overwrites_before_usage id tl
-    )
-      | TAC_int     (left, _)
-      | TAC_str     (left, _)
-      | TAC_new     (left, _)
-      | TAC_default (left, _)
-      | TAC_bool    (left, _) -> (
-        if id = left then true 
-        else overwrites_before_usage id tl
-    )
-      | TAC_ident   (left, a)
-      | TAC_neg     (left, a)
-      | TAC_isvoid  (left, a)
-      | TAC_not     (left, a) -> (
-        if id = left && id != a then true 
-        else if id = a then false
-        else overwrites_before_usage id tl
-    )
-      | TAC_call    (left, s, args) -> false (* no inlining yet *)
-      | TAC_comment   s -> overwrites_before_usage id tl
-      (* everything else shouldn't be reached or destroys all liveness info *)
-      | x -> false
-  )
+let rec overwrites_before_usage (id : tac_id) (lst : tac_cmd list) : bool =
+match lst with
+| [] -> false
+| hd :: tl ->
+  Printf.printf "Checking if id %s is overwritten or used by instruction %s\n"
+    (p_id id) (string_of_tac_cmd hd);
+  let is_used = match hd with
+    | TAC_add (_, a, b)
+    | TAC_sub (_, a, b)
+    | TAC_mul (_, a, b)
+    | TAC_div (_, _, a, b)
+    | TAC_lt  (_, a, b)
+    | TAC_lte (_, a, b)
+    | TAC_eq  (_, a, b) -> id = a || id = b
+    | TAC_neg (_, a)
+    | TAC_not (_, a)
+    | TAC_isvoid (_, a)
+    | TAC_ident (_, a) -> id = a
+    | TAC_call (_, _, args) -> List.exists ((=) id) args
+    | _ -> false
+  in
+  if is_used then false
+  else
+    let is_overwritten = match hd with
+      | TAC_add (lhs, _, _)
+      | TAC_sub (lhs, _, _)
+      | TAC_mul (lhs, _, _)
+      | TAC_div (_, lhs, _, _)
+      | TAC_lt  (lhs, _, _)
+      | TAC_lte (lhs, _, _)
+      | TAC_eq  (lhs, _, _)
+      | TAC_neg (lhs, _)
+      | TAC_not (lhs, _)
+      | TAC_isvoid (lhs, _)
+      | TAC_int (lhs, _)
+      | TAC_str (lhs, _)
+      | TAC_bool (lhs, _)
+      | TAC_ident (lhs, _)
+      | TAC_new (lhs, _)
+      | TAC_default (lhs, _) -> id = lhs
+      | _ -> false
+    in
+    if is_overwritten then true else overwrites_before_usage id tl
 
 
-let is_instruction_dead (instruction : tac_cmd) (lst : tac_cmd list) : bool = 
-  Printf.printf "Checking if instruction is a candidate %s \n" (string_of_tac_cmd instruction);
-  match instruction with 
-  | TAC_int (lhs, _)
-  | TAC_bool (lhs, _)
-  | TAC_str (lhs, _) 
-  | TAC_ident (lhs, _) 
-  | TAC_add (lhs, _,  _)
-  | TAC_sub (lhs, _,  _)
-  | TAC_mul (lhs, _,  _)
-  | TAC_div (_, lhs, _,  _)
-  | TAC_lt (lhs, _,  _)
-  | TAC_lte (lhs, _,  _)
-  | TAC_eq (lhs, _,  _)
-  | TAC_neg (lhs, _)
-  | TAC_not (lhs, _)
-  (* Not sure about new or default yet - will this break some assembly code? *)
-  (* | TAC_new (lhs, _) *)
-  (* | TAC_default (lhs,  _) *)
-  | TAC_isvoid (lhs,  _) -> (
-    print_endline "YES candidate";
-    if overwrites_before_usage lhs lst then true else false
-  )
-  | _ -> print_endline "NO candidate"; false 
+
+  let is_instruction_dead (instruction : tac_cmd) (rest : tac_cmd list) : bool =
+    Printf.printf "Checking if instruction is dead: %s\n" (string_of_tac_cmd instruction);
+    match instruction with
+    | TAC_int (lhs, _)
+    | TAC_bool (lhs, _)
+    | TAC_str (lhs, _)
+    | TAC_ident (lhs, _)
+    | TAC_add (lhs, _, _)
+    | TAC_sub (lhs, _, _)
+    | TAC_mul (lhs, _, _)
+    | TAC_div (_, lhs, _, _)
+    | TAC_lt (lhs, _, _)
+    | TAC_lte (lhs, _, _)
+    | TAC_eq (lhs, _, _)
+    | TAC_neg (lhs, _)
+    | TAC_not (lhs, _)
+    | TAC_isvoid (lhs, _)
+    | TAC_default (lhs, _) ->
+      overwrites_before_usage lhs rest
+    | _ -> false
+  
  
 
-let local_dce (block : basic_block) : tac_cmd list = 
-  let rec check_instructions (l : tac_cmd list) : tac_cmd list = 
-    match l with
+let local_dce (block : basic_block) : tac_cmd list =
+  Printf.printf "Processing block %d for local DCE\n" block.id;
+  let rec remove_dead = function
     | [] -> []
-    | hd :: tl -> (
-      match hd with
-      | TAC_int (lhs, _) | TAC_bool(lhs, _) | TAC_str (lhs, _) | TAC_ident (lhs, _) -> (
-        Printf.printf "Checking instruction %s \n" (string_of_tac_cmd hd);
-          if is_instruction_dead hd tl then (print_endline "DEAD\n"; check_instructions tl )
-          else (print_endline "NOT dead\n"; hd :: check_instructions tl)
-      )
-      | _ -> (
-        hd :: check_instructions tl
-      )
-    )
+    | instr :: rest ->
+      if is_instruction_dead instr rest then
+        (Printf.printf "Removing dead instruction: %s\n" (string_of_tac_cmd instr);
+          remove_dead rest)
+      else
+        instr :: remove_dead rest
   in
-  check_instructions block.instructions
+  remove_dead block.instructions
+  
 
 let dce_method (mthd : method_cfg) : method_cfg = 
   (* Local DCE *)
@@ -93,9 +91,6 @@ let dce_method (mthd : method_cfg) : method_cfg =
     let updated_block = { block with instructions = updated_tac_cmds } in
     Hashtbl.replace mthd.blocks id updated_block
   ) mthd.blocks;
-
-  (* Blocks with no sucessors are dead *)
-
 
   (* Global DCE *)
   mthd
